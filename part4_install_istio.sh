020c84cc7f35279df509742c11a6f562faec96d4
arty <keith.elam.guru@gmail.com>
2020-01-26 13:16:29 -0500

    changed install_istio script

--- part4_install_istio.sh
+++ part4_install_istio.sh
@@ -4,14 +4,14 @@
 # site: https://programmaticponderings.com
 # license: MIT License
 # purpose: Install Istio 1.1.x
-export ISTIO_HOME="/home/arty/Work/K8S/istio-1.4.2"
+#          make sure to 'export ISTIO_HOME'
 
 # helm repo add istio.io https://storage.googleapis.com/istio-prerelease/daily-build/master-latest-daily/charts
 # helm repo list
 
 kubectl apply -f ${ISTIO_HOME}/install/kubernetes/helm/helm-service-account.yaml
 
-# helm init --service-account tiller
+helm init --service-account tiller
 
 # Wait for Tiller pod to come up
 # Error: could not find a ready tiller pod
@@ -19,15 +19,15 @@ echo 'Waiting 30 seconds...'
 sleep 30
 
 helm install ${ISTIO_HOME}/install/kubernetes/helm/istio-init \
-  --generate-name \
+  --name istio-init \
   --namespace istio-system
 
 # Wait for AKS, much slower than GKE :(
-# echo 'Waiting 30 seconds...'
-# sleep 30
+echo 'Waiting 30 seconds...'
+sleep 30
 
 helm install ${ISTIO_HOME}/install/kubernetes/helm/istio \
-  --generate-name \
+  --name istio \
   --namespace istio-system \
   --set prometheus.enabled=true \
   --set grafana.enabled=true \
